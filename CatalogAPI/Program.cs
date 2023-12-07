f6e00ec3292d83c252f94f13c449ea5616615037
gabrielosantosb <email@email.com>
2023-12-07 16:05:39 -0300

    Melhorando classe program

--- CatalogAPI/Program.cs
+++ CatalogAPI/Program.cs
@@ -18,72 +18,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "apiagenda", Version = "v1" });
-
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() 
-                { 
-                    Name = "Authorization", 
-                    Type = SecuritySchemeType.ApiKey, 
-                    Scheme = "Bearer", 
-                    BearerFormat = "JWT", 
-                    In = ParameterLocation.Header,
-                   // Description = "JWT Authorization header using the Bearer scheme.
-                   //\r\n\r\n Enter 'Bearer'[space] and then your token in the text input below.
-                   // \r\n\r\nExample: \"Bearer 12345abcdef\"",
-                }); 
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement 
-                { 
-                    { 
-                          new OpenApiSecurityScheme 
-                          { 
-                              Reference = new OpenApiReference 
-                              { 
-                                  Type = ReferenceType.SecurityScheme, 
-                                  Id = "Bearer" 
-                              } 
-                          }, 
-                         new string[] {} 
-                    } 
-                }); 
-   });
-builder.Services.AddAuthorization();
+builder.AddApiSwagger();
+builder.AddPersistence();
+builder.Services.AddCors();
+builder.AddAuthJWT();
 builder.Services.AddCors();
 
 
 
 builder.Services.AddScoped<IViaCepIntegration, ViaCepIntegration>();
-builder.Services.AddSingleton<ITokenService>(new TokenService());
+
 builder.Services.AddRefitClient<IViaCepIntegracaoRefit>().ConfigureHttpClient(httpClient =>
     httpClient.BaseAddress = new Uri("https://viacep.com.br"));
 
 
-//--------------------------ValidarToken-----------------------------------
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey
-        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
-
-
-
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-ServerVersion serverVersion = ServerVersion.AutoDetect(connectionString);
-
-builder.Services.AddDbContext<MySQLContext>(options =>
-    options.UseMySql(connectionString, serverVersion));
 
 var app = builder.Build();
 app.MapAuthentificateEndpoints();
